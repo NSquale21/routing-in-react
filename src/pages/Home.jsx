ccfefa47810e868cb0bbb9e402235b1507dd1ef7
Nicole Pasquale <NSquale212@gmail.com>
2020-01-28 19:22:58 -0600

    Fix className typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = props => {
   return (
     <section className="d-flex justify-content-center my-5">
-	    <img src="https://ghibliapi.herokuapp.com/images/logo.svg" class="img-fluid" alt="Studio Ghibli Logo" />
+	    <img src="https://ghibliapi.herokuapp.com/images/logo.svg" className="img-fluid" alt="Studio Ghibli Logo" />
 		</section>
   );
 }

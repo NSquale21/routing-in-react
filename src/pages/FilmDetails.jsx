4d270d7a1bdd88f2d25d2c03ec4560dc89c25d7f
Nicole Pasquale <NSquale212@gmail.com>
2020-01-28 19:23:57 -0600

    Add error handling

--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { FaExclamationCircle } from 'react-icons/fa';
 
 const FilmDetails = props => {
 	
 	const [film, setFilm] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		const getFilm = async () => {
 			let res = await fetch('https://ghibliapi.herokuapp.com/films/' + props.match.params.id);
-			let film = await res.json();
-			setFilm(film);
-		}
+			if (res.ok) {
+				let film = await res.json();
+				setFilm(film);
+			} else {
+				setError(true);
+			}
+		};
 		getFilm();
-	}, []);
+	}, [props.match.params.id]);
 	
 	return (
 		<section className="row justify-content-center my-3">
 			<article className="col-md-8">
 				<div className="card my-3 shadow">
 					<div className="card-body text-center">
-						<h4 className="card-title">{film.title}</h4>
+						{error ? <h1 className="text-danger"><FaExclamationCircle /></h1> : <h4 className="card-title">{film.title}</h4>}
 						<p className="card-text">{film.description}</p>
 					</div>
 				</div>
--- src/pages/PeopleDetails.jsx
+++ src/pages/PeopleDetails.jsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { FaExclamationCircle } from 'react-icons/fa';
 
 const PeopleDetails = props => {
 	
 	const [person, setPerson] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		const getPerson = async () => {
 			let res = await fetch('https://ghibliapi.herokuapp.com/people/' + props.match.params.id);
-			let person = await res.json();
-			setPerson(person);
+			if (res.ok) {
+				let person = await res.json();
+				setPerson(person);
+			} else {
+				setError(true);
+			}
 		}
 		getPerson();
-	}, []);
+	}, [props.match.params.id]);
 	
 	return (
 		<section className="row justify-content-center my-3">
 			<article className="col-md-8">
 				<div className="card my-3 shadow">
 					<div className="card-body text-center">
-						<h4 className="card-title">{person.name}</h4>
+						{error ? <h1 className="text-danger"><FaExclamationCircle /></h1> : <h4 className="card-title">{person.name}</h4>}
 						<p className="card-text">Age: {person.age ? person.age : 'NA'}</p>
 					</div>
 				</div>
